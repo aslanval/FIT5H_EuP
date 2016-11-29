a44a20386cbfba4e7bb72a3412b156cfb77cc928
aslanval <valid.aslan@gmx.de>
2016-11-29 10:31:27 +0100

    29.11.2016 10:31

--- Rails/Projekte/rails_app/db/seeds.rb
+++ Rails/Projekte/rails_app/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Event.create(name: "Sommerferien", description: "Schiwtzen, baden, schwitzen", start_date: 100.days.from_now, location: "Hamburg", price: 1000.00)
+Event.create(name: "Weihnachtsball", description: "Tanzen, lustig sein, rauchen, unanstaendige Gedanken haben", start_date: "2016-12-31", location: "Bayern", price: 30.00)
+Event.create(name: "Whisky trinken", description: "An gerade Tage peated, sonst: unpeated", start_date: "2016-11-29", location: "Scotland", price: 50.00)
+
