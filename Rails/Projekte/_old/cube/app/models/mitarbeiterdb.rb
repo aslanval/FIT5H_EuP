ffce0648f7d2f14f1f8e140752daa5ac73ee833a
aslanval <valid.aslan@gmx.de>
2017-01-03 21:55:36 +0100

    Cube 03.01.2017

--- Rails/Projekte/_old/cube/app/models/mitarbeiterdb.rb
+++
@@ -1,2 +0,0 @@
-class Mitarbeiterdb < ApplicationRecord
-end
--- Rails/Projekte/_old/cube/test/controllers/cubes_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CubesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
