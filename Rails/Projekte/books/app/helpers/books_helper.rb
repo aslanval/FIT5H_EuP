372752ee09b0a20459b1bd172cdb0791bb60e5d3
aslanval <valid.aslan@gmx.de>
2017-01-16 15:04:10 +0100

    16.01.2017 Schule

--- Rails/Projekte/books/app/helpers/books_helper.rb
+++ Rails/Projekte/books/app/helpers/books_helper.rb
@@ -11,4 +11,4 @@ module BooksHelper
 		end
 	end
 end
- 
+   
--- Rails/Projekte/books/config/routes.rb
+++ Rails/Projekte/books/config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   	resources :books do
   		resources :likes
   	end
-  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
