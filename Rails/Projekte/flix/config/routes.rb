444ed2c27d78cea5c88880c62a4ec3a22d3e515f
aslanval <valid.aslan@gmx.de>
2017-01-24 13:00:26 +0100

    LogIn/Session

--- Rails/Projekte/flix/config/routes.rb
+++ Rails/Projekte/flix/config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+	root 'movies#index'
+	get 'signup' => 'users#new'
+	
+	resource :session
+  resources :users
 	resources :movies do
 		resources :reviews
 	end	
-
-root 'movies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 end
--- Rails/Projekte/rails_app/config/routes.rb
+++ Rails/Projekte/rails_app/config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+
+	root 'events#index'
+
+	get 'signup' => 'users#new'
+
+	resource :session
+  resources :users
 	resources :events do
  		resources :registrations
  	end
 
-root 'events#index' #zeigt die events/index.html.erb an wenn localhost:3000 geöffnet wird
+ #zeigt die events/index.html.erb an wenn localhost:3000 geöffnet wird
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get "events" => "events#index"
   #get "events/:id" => "events#show"
-end
+end 
